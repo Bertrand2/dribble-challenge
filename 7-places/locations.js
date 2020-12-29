d0de42083094cc40981919bd43373b90cf4383f7
Bertrand2 <dossogne.bertrand@gmail.com>
2020-12-29 09:45:47 +0100

    [WIP] Places

---
+++ 7-places/locations.js
@@ -0,0 +1,205 @@
+"use strict";
+
+const locations = [
+	[
+		{
+			lat: 50.62242, 
+			long: 5.58300, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58310, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58320, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58330, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58340, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58350, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58360, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58370, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58380, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"
+		]}
+	], [
+		{
+			lat: 50.62242, 
+			long: 5.58300, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58310, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58320, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58330, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58340, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58350, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"
+		]}
+	], [
+		{
+			lat: 50.62242, 
+			long: 5.58300, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58400, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58500, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58600, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58700, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58800, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58900, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"
+		]}
+	], [
+		{
+			lat: 50.62242, 
+			long: 5.58300, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"]
+		}, {
+			lat: 50.62242, 
+			long: 5.58310, 
+			cat: "Restaurant", 
+			name: "Big Frite", 
+			address: "116, rue Saint-Denis, 4000 Liège", 
+			tags: ["Frites", "Liège", "Vegan Option"], 
+			img: ["bigFrite1","bigFrite2", "bigFrite3"
+		]}
+	]
+]
--- 7-places/script.js
+++ 7-places/script.js
@@ -1,2 +1,40 @@
 "use strict";
 
+const openMenu = () => {
+	document.getElementById("home").classList.add("opened");
+	setTimeout(() => {
+		document.getElementById("homeBackground").classList.add("opened");
+	}, 200);
+}
+
+const showFavorites = () => {
+	document.getElementById("favorites").classList.remove("opened");
+}
+const insertLocations = (id) => {
+	//mymap.removeLayer(marker);
+	document.querySelector("div.leaflet-shadow-pane").innerHTML = "";
+	document.querySelector("div.leaflet-marker-pane").innerHTML = "";
+
+	locations[id].forEach((location) => {
+		const marker = L.marker([location.lat, location.long]).addTo(mymap);
+	});
+}
+
+document.getElementById("login").addEventListener("click", ()=>{openMenu()});
+document.getElementById("signup").addEventListener("click", ()=>{openMenu()});
+document.getElementById("list").addEventListener("click", ()=>{showFavorites()});
+
+[...document.querySelectorAll("button.favorites__category")].forEach((button, i) => {
+	button.querySelector(".favorites__category-amount").innerHTML = locations[i].length;
+	button.addEventListener("click", () => {
+		document.getElementById("favorites").classList.add("opened");
+		insertLocations(i);
+	});
+});
+
+
+//LEAFLET
+let mymap = L.map('mapid').setView([50.62242, 5.58360], 13);
+
+const layer =  new L.StamenTileLayer("terrain");
+mymap.addLayer(layer);
